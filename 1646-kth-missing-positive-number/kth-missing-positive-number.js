eaaaebef9a92d8167d845126e9fb13c38da4d353
Devesh Sharma <59760503+nerdlyDev@users.noreply.github.com>
2025-01-08 00:06:53 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (40.26%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.js
@@ -5,14 +5,30 @@
  */
 var findKthPositive = function(arr, k) {
     // The brute force
-    for (let i = 0; i<= arr.length; i++)
-    {
-        if (arr[i] <= k)
-        {
-            k += 1;
-        } else {
-            break;
+    // for (let i = 0; i<= arr.length; i++)
+    // {
+    //     if (arr[i] <= k)
+    //     {
+    //         k += 1;
+    //     } else {
+    //         break;
+    //     }
+    // }
+    // return k;
+
+    // The optimal approach
+    let low = 0, high = arr.length - 1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let missing = arr[mid] - (mid + 1);
+        if (missing < k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
         }
     }
-    return k;
+    return k + high + 1;
+
 };
+
