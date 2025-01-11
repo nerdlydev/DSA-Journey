51e231d37066cc4abc6341a5fdb6673932b24e73
Devesh Sharma <59760503+nerdlyDev@users.noreply.github.com>
2025-01-12 00:00:25 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (62.64%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        // The Brute Force:
-        int n = nums.size();
-        if(n == 0) return;
-      //  k = k % n;
-        //if (k > n)
-        //   return;
-        int temp[n]; // Temp array for rotating 
-        for(int i = 0; i < n; i++){
-            temp[(i+k)%n] = nums[i];
-        }
-        // coping back to the orignal array
-        for(int j = 0; j< n; j++){
-            nums[j] = temp[j];
-        }
+// The Brute Force 
+        // int n = nums.size();
+        // if(n == 0) return;
+        // k = k % n;
+        // if (k > n)
+        //     return;
+        // int temp[k]; // required temp array of size k 
+        // // we start shifting from kth index
+        // for(int i = k; i < n; i++){
+        //     nums[i - k] = nums[i];
+        // }
+
+        // // puting back temp array
+
+        // for (int i = n - k; i < n; i++){
+        //     nums[i] = temp[i - (n - k)];
+        // }
+
         // Optimal solution : 
 
-        // k %= nums.size();
-        // reverse(nums.begin(), nums.end());
-        // reverse(nums.begin(), nums.begin() + k);
-        // reverse(nums.begin() + k, nums.end());
+        k %= nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
+        
+
 };
