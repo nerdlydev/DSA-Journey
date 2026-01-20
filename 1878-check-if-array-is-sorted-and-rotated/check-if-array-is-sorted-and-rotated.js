91299302442b7d9d5b6f1fc4b6643b0a5350a5a4
Dev.esh <developersharma1999@gmail.com>
2026-01-21 00:05:34 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (40.33%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -3,15 +3,16 @@
  * @return {boolean}
  */
 var check = function(nums) {
-    let cnt = 0;
-     for (let i = 0; i<nums.length; i++){
-         if(nums[i] > nums[(i + 1) % nums.length]){
-             cnt++;
-         }
-         if(cnt > 1){
-             return false;
-         }
-     }   
-     return true;
-  
+    const n = nums.length;
+    let count = 1;
+    for (let i = 1; i <= 2*n; i++)
+    {
+        if(nums[(i - 1) % n] <= nums[i % n]) {
+            count += 1;
+        }
+        else count = 1;
+
+        if (count === n) return true; 
+    }
+    return n === 1;
 };
